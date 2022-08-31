1d21eccb704ce45e931ca7ffc6d1aed86da3d203
Konstantin BIFERT <konstantin.bifert@gmail.com>
2022-08-31 09:53:55 +0200

    meh

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <Renderer ref="rendererC" antialias :orbit-ctrl="{ enableDamping: true }" resize="window">
-    <Camera ref="cameraC" :fov="30" :look-at="{ x: 0 }" :position="{ x: 0, y: 3, z: 50 }" />
-    <Scene ref="sceneC" background="#333">
+    <Camera ref="cameraC" :fov="15" :position="{ x: 0, y: 90, z: 100 }" />
+    <Scene ref="sceneC" background="#111">
       <PointLight :position="{ y: 10, z: 50 }" color="#fff" :intensity="0.7" />
-      <PointLight :position="{ y: -10, z: -50 }" color="#fff" :intensity="0.7" />
 
-      <Mesh ref="meshC" :position="{ x: 0, y: 0}" :rotation="{ z: 0, x: 90 }">
+      <!-- <Mesh ref="meshC" :position="{ x: 0, y: 0}" :rotation="{ z: 0, x: 90 }">
         <PlaneGeometry :width="40" :height="40" />
-      </Mesh>
-      <Box :size="1">
+      </Mesh> -->
+      <Box ref="boxC" :scale="{ x: 5, y: 2, z: 1 }" :rotation="{ x: Math.PI / 2, y: Math.PI / 2, z: -Math.PI / 2 }">
+        <!-- :rotation="{ x: Math.PI / 2, y: Math.PI / 2, z: -Math.PI / 2 }" -->
         <PhongMaterial color="#FF5F15" />
       </Box>
     </Scene>
@@ -17,36 +17,73 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { onKeyStroke } from '@vueuse/core'
-
+// import { onKeyStroke } from '@vueuse/core'
 import { Box, Camera, PhongMaterial, Mesh, PlaneGeometry, PointLight, Renderer, Scene } from 'troisjs'
-import { GridHelper, DoubleSide } from 'three'
+import { GridHelper } from 'three'
 
 const cameraC = ref()
 const rendererC = ref()
-const meshC = ref()
+// const meshC = ref()
 const sceneC = ref()
+const boxC = ref()
+
+let currentDirectionIndex = ref(0)
+
+const directions = [
+  { axis: 'X', multiplier: 1, name: 'front' },
+  { axis: 'Z', multiplier: 1, name: 'right' },
+  { axis: 'X', multiplier: -1, name: 'bottom' },
+  { axis: 'Z', multiplier: -1, name: 'left' }
+]
+const getCurrentDirection = (index) => directions.at(index % 4)
 
 onMounted(() => {
   const renderer = rendererC.value
-  const mesh = meshC.value.mesh
+  // const orbitControls = renderer.three.cameraCtrl
+  // const mesh = meshC.value.mesh
   const camera = cameraC.value.camera
   const scene = sceneC.value.scene
+  const box = boxC.value.mesh
 
-  const grid = new GridHelper(40, 40)
+  const grid = new GridHelper(40, 40, 0x00A8C9, 0x00A8C9)
   scene.add(grid)
-  console.log('camera', camera)
+  // console.log('eh', orbitControls)
+  // orbitControls.enablePan = false
+  // camera.rotation.x = 220
+  // camera.lookAt(box.position.x, box.position.y, box.position.z)
 
   renderer.onBeforeRender(() => {
-    mesh.rotation.z += 0.02
-    camera.rotation.x += 0.02
+    // console.log('ee', getCurrentDirection(currentDirectionIndex.value).name)
+
+    const getCurrent = getCurrentDirection(currentDirectionIndex.value)
+    console.log('cur', getCurrent)
+    box[`translate${getCurrent.axis}`](0.02 * getCurrent.multiplier)
+    // box['translateZ'](0.02)
+    // box.translateOnAxis(box.position.y, 0.02)
+    //! get direction
+    // +x front -x bottom
+    // -z left +z right
+
+
+    // camera.rotation.x += 90 * Math.PI / 180
+    // orbitControls.object.rotation.y += 90
+    // camera.lookAt({ x: 0, y: 12, z: 7})
+    // camera.rotation.x += 90
+    // orbitControls.object.rotation.y += 90
+    // orbitControls.update()
+    // renderer.render(scene, camera)
   })
-  // console.log('c', cameraC.value.camera.rotation)
-  onKeyStroke('ArrowDown', (e) => {
+
+  document.addEventListener('keydown', (e) => {
     e.preventDefault()
-    camera.rotation.z += 0.2
-    console.log('nice')
-  })
+    if (e.key === 'ArrowLeft') {
+      // box.rotateY(45 * (Math.PI / 2))
+      currentDirectionIndex.value--
+    } else if (e.key === 'ArrowRight') {
+      // box.rotateY(-45 * (Math.PI / 2))
+      currentDirectionIndex.value++
+    }
+});
 })
 
 </script>
@@ -55,6 +92,7 @@ onMounted(() => {
 body {
   margin: 0;
 }
+
 canvas {
   display: block;
 }
