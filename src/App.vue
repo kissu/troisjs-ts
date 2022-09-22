6f88800e207d3f0e14a092f7ffd63a634eb42d61
Konstantin BIFERT <konstantin.bifert@gmail.com>
2022-09-22 11:30:29 +0200

    save

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <Renderer ref="rendererC" antialias :orbit-ctrl="{ enableDamping: true }" resize="window">
-    <Camera ref="cameraC" :fov="15" :position="{ x: 0, y: 90, z: 100 }" />
-    <Scene ref="sceneC" background="#111">
-      <PointLight :position="{ y: 10, z: 50 }" color="#fff" :intensity="0.7" />
-      <GltfModel ref="boxC" :rotation="{ y: Math.PI, z: 0 }"
-        :scale="{ x: 0.5, y: 0.5, z: 0.5 }" src="./tron.gltf" @error="onError" />
-    </Scene>
-  </Renderer>
+  <div style="position: relative;">
+    <Renderer ref="rendererC" antialias :orbit-ctrl="{ enableDamping: true }" resize="window">
+      <Camera ref="cameraC" :fov="15" :position="{ x: 0, y: 90, z: 100 }" />
+      <Scene ref="sceneC" background="#111">
+        <PointLight :position="{ y: 10, z: 50 }" color="#fff" :intensity="0.7" />
+        <GltfModel ref="boxC" :rotation="{ y: Math.PI, z: 0 }" :scale="{ x: 0.5, y: 0.5, z: 0.5 }" src="./tron.gltf"
+          @error="onError" />
+      </Scene>
+    </Renderer>
+    <video ref="webcam" style="position: absolute; top: 0; right: 0; height: 20vh; width: 20vw;" :srcObject="videoSrc">
+      nice meme</video>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { GltfModel, Box, Camera, PhongMaterial, Mesh, PlaneGeometry, PointLight, Renderer, Scene } from 'troisjs'
 import { GridHelper, InstancedMesh } from 'three'
+import * as pose from '@tensorflow-models/pose-detection'
+import '@tensorflow/tfjs-backend-webgl';
+
+const sleep = async (amount) => await new Promise((resolve) => setTimeout(resolve, amount))
 
 const cameraC = ref()
 const rendererC = ref()
 const sceneC = ref()
+const webcam = ref()
+let videoSrc = ref()
 
 let currentDirectionIndex = ref(0)
 
@@ -32,20 +42,61 @@ const onError = (e) => {
   console.log('error', e)
 }
 
-onMounted(() => {
+onMounted(async () => {
   const renderer = rendererC.value
   const camera = cameraC.value.camera
   const scene = sceneC.value.scene
   const moto = scene.children[2]
 
+  const videoConfig = {
+    audio: false,
+    video: {
+      facingMode: 'user',
+      width: 320,
+      height: 240,
+      frameRate: {
+        ideal: 60,
+      }
+    }
+  }
+  const stream = await navigator.mediaDevices.getUserMedia(videoConfig)
+  videoSrc.value = stream
+  await webcam.value.load()
+  webcam.value.play()
+
+  const model = pose.SupportedModels.MoveNet;
+  const detectorConfig = {
+    // modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING,
+    enableTracking: true,
+    // trackerType: poseDetection.TrackerType.BoundingBox
+  }
+  const detector = await pose.createDetector(model, detectorConfig);
+  // await sleep(2000)
+
   const grid = new GridHelper(40, 40, 0x00A8C9, 0x00A8C9)
   scene.add(grid)
 
-  renderer.onBeforeRender(() => {
+  renderer.onBeforeRender(async () => {
     scene.children[2]['translateZ'](0.02)
     //! directions
     // +x front -x bottom
     // -z left +z right
+    // const poses = await detector.estimatePoses(webcam.value)
+    // const leftWrist = poses[0].keypoints[9].y
+    // const rightWrist = poses[0].keypoints[10].y
+    // const direction = leftWrist / rightWrist
+    // console.log('d', direction)
+    // switch (true) {
+    //   case (direction > 2):
+    //     console.log('turn left')
+    //     break;
+    //   case (direction < 0.5):
+    //     console.log('turn right')
+    //     break;
+    //   default:
+    //     console.log('straight')
+    //     break;
+    // }
   })
 
   document.addEventListener('keydown', (e) => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// @ts-nocheck
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), mkcert()],
 })
